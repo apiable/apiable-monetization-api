3c774be761265facf9a8be2b62e2724a1fdbea96
Peter Porttinen <peter.porttinen@gmail.com>
2024-08-30 13:32:39 +0300

    rename update to archive to better represent it's function.

--- src/main/kotlin/io/apiable/monetization/adapter/MonetizationPrice.kt
+++ src/main/kotlin/io/apiable/monetization/adapter/MonetizationPrice.kt
@@ -22,7 +22,7 @@ import io.apiable.monetization.adapter.model.RevenueModel
 
 interface PriceWrapper {
     fun create(monetizationData: BillingPriceCreate): BillingPrice
-    fun retrieve(integrationId: String, revenueModel: RevenueModel): BillingPrice
-    fun update(integrationId: String, monetizationData: BillingPriceCreate): BillingPrice
+    fun retrieve(integrationId: String): BillingPrice
+    fun archive(integrationId: String): BillingPrice
     fun retrieveByLookupKey(lookupKey: String): BillingPrice
 }
